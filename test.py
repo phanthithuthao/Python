a68c336dadb17214a5572c69ff5d0bcf83c9ad51
Krist <phanthithuthao0204@gmail.com>
2023-07-05 13:50:48 +0700

    Update Repo Python

--- test.py
+++ test.py
@@ -2,9 +2,13 @@ x = 5
 y = "John"
 print(x)
 print(y)
-temp = input("nhap mot cau: ")
+#eval sẽ tự động chuyển sang số còn không có thì là chuỗi
+temp = eval(input("Nhap vao ngau nhien"))
 print(temp)
-temp = input("nhap mot cau: ")
-print(temp)
-temp = temp
-print(temp)
+#In ra kieu du lieu ne!!!
+"""
+Hello mọi người 
+xin chào mọi đã đến với khóa học Python 
+của thầy Trường
+"""
+print(type(temp))
