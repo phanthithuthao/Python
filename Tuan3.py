791c0c32aa959749f24b270df6f82338409885e9
Krist <phanthithuthao0204@gmail.com>
2023-07-18 20:37:33 +0700

    Update Tuan 3

--- Tuan3.py
+++ Tuan3.py
@@ -16,3 +16,16 @@ print(s.replace(x,y))
 print(s.count('o'))
 #Location of first character o
 print(s.index('o'))
+#Check alphabet (a-z)
+text = 'Company'
+flag = text.isalpha()
+print(flag)
+
+#Example Input a string if have character a print location a
+s2 = input("Enter a String: ")
+for i in range(len(s2)):
+    if s2[i]=='a' :
+        print(i,end="\t")
+#Example 2
+s3 =input("Enter a string: ")
+print(len(s3))
